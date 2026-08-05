640a5e8a27f15ae71a82798b1c5b20726ccb16d3
menna <menaelsayed868@gmail.com>
2026-08-05 18:21:14 +0300

    add faculty model

---
+++ server/models/faculty.js
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+import department from "./department";
+import { type } from "node:os";
+const facultySchema = mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+
+    },
+    avatar:{
+        type:String
+    },
+    password:{
+        type:String
+
+    },
+    username:{
+        type:String
+    },gender:{
+        type:String
+    },designation:{
+        type:String,
+        required:true
+    },
+    department:{
+        type:String,
+        required:true   
+    },
+    contactNumber:{
+        type:Number
+    },dob:{
+        type:String,
+        required:true
+    },
+    joiningYear:{
+        type:Number,
+        required:true
+    },
+    passwordUpdated:{
+        type:Boolean,
+        default:false
+    }
+})
+
+export default mongoose.model("faculty",facultySchema);
