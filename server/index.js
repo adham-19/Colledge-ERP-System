8a05d7a77e1d3eb62700511f610893035cd83b4b
Adham Mansour <ad7am798@gmail.com>
2026-08-05 16:23:27 +0300

    structure of the project

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+const app = express();
+dotenv.config();
+
+app.use(function (req, res) {
+  res.status(404).json("Page Not Found");
+});
+
+mongoose
+  .connect(process.env.MONGOURL)
+  .then(() => {
+    console.log("db connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.listen(process.env.PORT, () => {
+  console.log("server is running on port 3000");
+});
