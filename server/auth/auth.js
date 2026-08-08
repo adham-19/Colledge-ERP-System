fe19d5c705e854d47e04d318ebb05ade4ad0f812
Adham Mansour <ad7am798@gmail.com>
2026-08-08 16:27:39 +0300

    Add Auth file

---
+++ server/auth/auth.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+
+const auth = (req, res, next) => {
+  try {
+    const header = req.headers.authorization;
+    if (!header) {
+      return res.status(401).json("");
+    }
+
+    const token = header.split(" ")[1];
+    const decoded = jwt.verify(token, process.env.SECRETKEY);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    res.status(401).json({ message: err.message });
+  }
+};
+
+module.exports = auth;
